0bea0c2d7d90dea6c0c4d43cc1469681779ff796
Brett Feltmate <brettfeltmate@MacBook-Air-6.local>
2024-10-29 16:54:08 -0300

    Some cleaning

--- experiment.py
+++ experiment.py
@@ -16,12 +16,7 @@ from klibs.KLEventInterface import TrialEventTicket as ET
 
 from random import choice, randrange
 
-# Color Constants
 WHITE = [255, 255, 255, 255]
-RED = [255, 0, 0, 255]
-
-# Anti-typo Constants
-# TODO: likely unnecessary
 TL = 'top_left'
 TR = 'top_right'
 BL = 'bottom_left'
@@ -31,20 +26,18 @@ BR = 'bottom_right'
 class line_discrimination_vigil(klibs.Experiment):
     @override
     def setup(self) -> None:
-        # Stimulus params
-
-        # FIX: move hardcoded values from make_array_locs to params
         self.params = {
             'line_length': deg_to_px(0.5),
             'fixation_width': deg_to_px(0.5),
             'stroke_width': deg_to_px(0.1),
+            'jitter_unit': deg_to_px(0.1),
             'jitter_bound': 5,
-            'jitter_mod': 2,
+            'target_offset': 11,  # NOTE: initial value; procedurally redetermined via performance checks
             'flanker_gap': deg_to_px(0.15),
             'array_locs': [-2, -1, 0, 1, 2],
             # in ms
             'inter_trial_interval': 2000,
-            'array_duration': 1000,
+            'array_duration': 250,
             'response_window': 1000,
         }
 
@@ -56,22 +49,27 @@ class line_discrimination_vigil(klibs.Experiment):
         h_offset = P.screen_c[0] // 2
         # possible locations for arrays to be centred on
         self.array_anchors = {
-            TL: [P.screen_c[0] - h_offset, P.screen_c[1] - v_offset],
-            TR: [P.screen_c[0] + h_offset, P.screen_c[1] - v_offset],
-            BL: [P.screen_c[0] - h_offset, P.screen_c[1] + v_offset],
-            BR: [P.screen_c[0] + h_offset, P.screen_c[1] + v_offset],
+            TL: (P.screen_c[0] - h_offset, P.screen_c[1] - v_offset),
+            TR: (P.screen_c[0] + h_offset, P.screen_c[1] - v_offset),
+            BL: (P.screen_c[0] - h_offset, P.screen_c[1] + v_offset),
+            BR: (P.screen_c[0] + h_offset, P.screen_c[1] + v_offset),
         }
+
+        self.line = kld.Line(
+            length=self.params['line_length'],
+            thickness=self.params['stroke_width'],
+            color=WHITE,
+            rotation=90,
+        )
 
     @override
     def block(self) -> None:
         fill()
 
-        # TODO:
-        # - actual instructions
-        # - performance feedback?
+        # TODO: instructions
         message(
             'Hit space for targets.\nAny key to start block.',
-            registration=7,
+            registration=5,
             location=P.screen_c,
             blit_txt=True,
         )
@@ -82,39 +80,28 @@ class line_discrimination_vigil(klibs.Experiment):
     @override
     def trial_prep(self) -> None:
         self.array_center = choice(list(self.array_anchors.values()))
-        self.array_locs = self.make_array_locs()
+        self.array = self.make_array()
 
         trial_events = []
-        trial_events.append(['array_off', self.params['array_duration']])
+        trial_events.append(['remove_array', self.params['array_duration']])
         trial_events.append(
             [
                 'response_timeout',
                 trial_events[-1][1] + self.params['response_window'],
             ]
         )
-        trial_events.append(['trial_end', self.params['inter_trial_interval']])
+        trial_events.append(['end_trial', self.params['inter_trial_interval']])
 
-        for te in trial_events:
-            self.evm.register_ticket(ET(te[0], te[1]))
-
-        # FIX:
-        # - move back into setup()
-        # - remove colour conditional
-        self.line = kld.Line(
-            length=self.params['line_length'],
-            thickness=self.params['stroke_width'],
-            color=WHITE if not self.target_trial else RED,
-            rotation=90,
-        )
+        for ev in trial_events:
+            self.evm.register_ticket(ET(ev[0], ev[1]))
 
     @override
     def trial(self) -> dict[str, Any]:
-        print(f'\nTarget trial: {self.target_trial}')
         rt_clock = Stopwatch()
         rt = None
 
         fill()
-        for loc in self.array_locs:
+        for loc in self.array:
             blit(self.line, registration=5, location=loc)
         flip()
 
@@ -154,7 +141,7 @@ class line_discrimination_vigil(klibs.Experiment):
     def clean_up(self) -> None:
         pass
 
-    def make_array_locs(self) -> list[tuple[int, int]]:
+    def make_array(self) -> list[tuple[int, int]]:
         locs = []
 
         # HACK: values hardcoded for now
