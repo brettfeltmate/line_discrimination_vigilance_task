a790c1a2ee384cd0ad05ec9ec35d60d3f60d31e3
Brett Feltmate <brettfeltmate@MacBook-Air-6.local>
2024-10-29 16:53:43 -0300

    Refactored array_loc to be an IV

--- ExpAssets/Config/line_discrimination_vigil_independent_variables.py
+++ ExpAssets/Config/line_discrimination_vigil_independent_variables.py
@@ -1,3 +1,13 @@
 from klibs.KLStructure import FactorSet
 
-exp_factors = FactorSet({'target_trial': [True, False, False, False]})
+exp_factors = FactorSet(
+    {'target_trial': [True, False, False, False]},
+    {
+        'array_location': [
+            'top_left',
+            'top_right',
+            'bottom_left',
+            'bottom_right',
+        ]
+    },
+)
